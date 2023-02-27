0e500ef835bbb909b28d962e6e842ad6e3452b64
Nier4ever <20170127nwl@gmail.com>
2023-02-27 20:41:51 +0800

    去除哈希

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import answerRecord from "@/views/user/AnswerRecord.vue";
 import myTest from "@/views/user/MyTest.vue";
@@ -31,7 +31,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
