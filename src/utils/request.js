e524537016ebdc57829defae840730064c64994e
Nier4ever <20170127nwl@gmail.com>
2023-02-28 20:48:17 +0800

    引入axios

---
+++ src/utils/request.js
@@ -0,0 +1,80 @@
+import axios from 'axios'
+import router from '@/router'
+import store from '../store'
+import { ElNotification } from 'element-plus'
+
+// 创建axios实例
+const service = axios.create({
+  baseURL: process.env.NODE_ENV === 'production' ? process.env.VUE_APP_BASE_API : '/', // api 的 base_url
+})
+
+// request拦截器
+service.interceptors.request.use(
+  config => {
+    config.headers['Content-Type'] = 'application/json'
+    return config
+  },
+  error => {
+    Promise.reject(error)
+  }
+)
+
+// response 拦截器
+service.interceptors.response.use(
+  response => {
+    return response.data
+  },
+  error => {
+    // 兼容blob下载出错json提示
+    if (error.response.data instanceof Blob && error.response.data.type.toLowerCase().indexOf('json') !== -1) {
+      const reader = new FileReader()
+      reader.readAsText(error.response.data, 'utf-8')
+      reader.onload = function(e) {
+        const errorMsg = JSON.parse(reader.result).message
+        ElNotification.error({
+          title: errorMsg,
+          duration: 5000
+        })
+      }
+    } else {
+      let code = 0
+      try {
+        code = error.response.data.status
+      } catch (e) {
+        if (error.toString().indexOf('Error: timeout') !== -1) {
+          ElNotification.error({
+            title: '网络请求超时',
+            duration: 5000
+          })
+          return Promise.reject(error)
+        }
+      }
+      console.log(code)
+      if (code) {
+        if (code === 401) {
+          store.dispatch('LogOut').then(() => {
+            // 用户登录界面提示
+            location.reload()
+          })
+        } else if (code === 403) {
+          router.push({ path: '/401' })
+        } else {
+          const errorMsg = error.response.data.message
+          if (errorMsg !== undefined) {
+            ElNotification.error({
+              title: errorMsg,
+              duration: 5000
+            })
+          }
+        }
+      } else {
+        ElNotification.error({
+          title: '接口请求失败',
+          duration: 5000
+        })
+      }
+    }
+    return Promise.reject(error)
+  }
+)
+export default service
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    port: 9000,
+    proxy: {
+      '/api': {
+        target: process.env.VUE_APP_BASE_API,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': 'api'
+        }
+      },
+      '/auth': {
+        target: process.env.VUE_APP_BASE_API,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/auth': 'auth'
+        }
+      }
+    }
+  }
 })
